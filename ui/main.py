896806d97aa8e7621c6686249cc426e9f83af866
Erick <122332446+ErickGHZ@users.noreply.github.com>
2023-10-09 17:50:39 -0600

    Create main.py

---
+++ ui/main.py
@@ -0,0 +1,17 @@
+import requests
+
+URI = "http://localhost:8000/v1/contactos"
+
+response = requests.get(URI)
+
+print(f"GET: {response.text}")
+print(f"GET: {response.status_code}")
+
+
+data = {"nombre": "string","email": "string"}
+
+response = requests.post(URI,json=data)
+
+print(f"POST: {response.text}")
+print(f"POST: {response.status_code}")
+
