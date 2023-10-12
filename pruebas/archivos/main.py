ec3ee8946e2c501c6eb7e4a716e47b91d6746b9b
Erick Gutierrez <1722110095@utectulancingo.edu.mx>
2023-10-12 23:43:11 +0000

    recortes

--- pruebas/archivos/main.py
+++ pruebas/archivos/main.py
@@ -1,51 +1,48 @@
-from typing import Annotated
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import HTMLResponse
-import os
+from fastapi import FastAPI, HTTPException, Query
+from PIL import Image
+from io import BytesIO
+from starlette.responses import StreamingResponse
 from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
 
+# Montar la carpeta "static"
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-upload_folder = "static"
-
-@app.post("/uploadfiles/")
-async def create_upload_files(
-    files: Annotated[list[UploadFile], File(description="Multiple files as UploadFile")]
+@app.get("/images/{image_name}")
+async def view_image(
+    image_name: str,
+    crop: str = Query(..., description="Parámetros de recorte en formato 'x1,y1,x2,y2'")
 ):
-    saved_files = []
-    for file in files:
-        file_extension = file.filename.split(".")[-1]
-        if file.content_type == "application/pdf":
-            subfolder = "pdf"
-            
-        elif file.content_type.startswith("image/"):
-            subfolder = "images"
-            
-        else:
-            subfolder = "other"
-        
-        file_path = os.path.join(upload_folder, subfolder, file.filename)
-        with open(file_path, "wb") as f:
-            f.write(file.file.read())
-        
-        saved_files.append(file.filename)
+    try:
+        # Construir la ruta completa de la imagen original
+        image_path = f"static/images/{image_name}"
 
+        # Verificar si la imagen existe
+        try:
+            im = Image.open(image_path)
+        except Exception:
+            raise HTTPException(status_code=404, detail="La imagen especificada no existe.")
 
-        localhost = 'https://8000-erickghz-apirestcontact-oqkax5z2oex.ws-us105.gitpod.io/static'
-        ruta = f"{localhost}/{subfolder}/{file.filename}"
-        
-    return {"message": "Archivos guardados correctamente", "saved_files": ruta}
+        # Realizar el recorte
+        x1, y1, x2, y2 = map(int, crop.split(','))
+        box = (x1, y1, x2, y2)
+        region = im.crop(box)
 
-@app.get("/")
-async def main():
-    content = """
-<body>
-<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
-<input type="submit">
-</form>
-</body>
-    """
-    return HTMLResponse(content=content)
+        # Crear un buffer de Bytes para almacenar la imagen recortada
+        output = BytesIO()
+        region.save(output, format="PNG")
+        output.seek(0)
+
+        # Guardar la imagen recortada en la carpeta 'editadas'
+        edited_image_name = f"editadas/{image_name}"
+        edited_image_path = f"static/{edited_image_name}"
+        region.save(edited_image_path, format="PNG")
+
+        # Devolver la URI para acceder a la imagen recortada
+        edited_image_uri = f"https://localhost:8000/static/{edited_image_name}"
+
+        return {"message": "Imagen recortada guardada en:", "edited_image_uri": edited_image_uri}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error al procesar la imagen: " + str(e))
